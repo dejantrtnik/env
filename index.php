f86d131218ff9f8aa820c19fbe8f42a20d9a2c20
dejantrtnik <dejantrtnik@gmail.com>
2020-11-13 11:30:03 +0100

    commits

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
+// just for errors 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 
-// samle Directory
+// sample Directory      
 $path = substr(realPath(__DIR__), 18);
 define($path, true);
 require_once 'application/Config/Env.php';
+//echo $path;
 
 // 'demo' some additional data for project
 $env = new Env('demo');
