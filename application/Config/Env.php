04c22d2ba91a070947f635d1a09880626f9817ac
dejantrtnik <dejantrtnik@gmail.com>
2020-11-13 11:11:56 +0100

    first commit

---
+++ application/Config/Env.php
@@ -0,0 +1,25 @@
+<?php
+// sample Directory
+if (!defined('/env')) {
+  die('403');
+}
+class Env
+{
+  function __construct($dotEnv){
+    $this->dotEnv = $dotEnv;
+  }
+  function dotEnv($dotEnv){
+    if (file_exists('.env')) {
+      $fileDotEnv = explode("\n", file_get_contents('.env', true));
+      foreach ($fileDotEnv as $envData) {
+        if(preg_match('/^' . $dotEnv . ' = /',$envData)){
+          $data = preg_replace('/^' . $dotEnv . ' = /', '' , $envData);
+          return $data;
+        }
+      }
+    }
+  }
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
+// samle Directory
+$path = substr(realPath(__DIR__), 18);
+define($path, true);
+require_once 'application/Config/Env.php';
+
+// 'demo' some additional data for project
+$env = new Env('demo');
+echo $env->dotEnv('db.default.hostname');
+
+?>
